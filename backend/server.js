03bc6c90c6049148743a2829e63ec4250a6f2fe0
Chris Mullins <mrchrismullins@gmail.com>
2024-08-02 11:07:51 -0500

    moved file and deploy test

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,32 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const productRoutes = require('./routes/products')
-const userRoutes = require('./routes/users')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const productRoutes = require("./routes/products");
+const userRoutes = require("./routes/users");
+const app = express();
+const port = 3000;
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
 
 mongoose
-  .connect('mongodb://localhost:27017/productDB', {})
+  .connect(
+    `mongodb+srv://mrchrismullins:${password}@elitecluster.grputig.mongodb.net/?retryWrites=true&w=majority&appName=EliteCluster`,
+    {}
+  )
   .then(() => {
-    console.log('Connected to Mongo')
+    console.log("Connected to Mongo");
   })
   .catch((error) => {
-    console.error('Mongo connections error:', error)
-  })
-app.use(cors())
-app.use(express.json())
-app.use('/getProducts', productRoutes)
-app.use('/users', userRoutes)
+    console.error("Mongo connections error:", error);
+  });
+app.use(cors());
+app.use(express.json());
+app.use("/getProducts", productRoutes);
+app.use("/users", userRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running on port:${port}`)
-})
+  console.log(`Server running on port:${port}`);
+});
